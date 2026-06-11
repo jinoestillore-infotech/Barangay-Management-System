bd4d27fa6d8eff7131f0e446eb2f68078c15ed29
jinoestillore-infotech <jinoestillore.infotech@gmail.com>
2026-06-12 01:39:46 +0800

    Added session timeout in auth

--- includes/auth.php
+++ includes/auth.php
@@ -2,7 +2,22 @@
 
 session_start();
 
+$timeout = 1800; // 30 minutes
+
 if (!isset($_SESSION['user_id'])) {
     header("Location: ../login.php");
     exit;
 }
+
+if (
+    isset($_SESSION['last_activity']) &&
+    (time() - $_SESSION['last_activity']) > $timeout
+) {
+    session_unset();
+    session_destroy();
+
+    header("Location: ../login.php?expired=1");
+    exit;
+}
+
+$_SESSION['last_activity'] = time();
