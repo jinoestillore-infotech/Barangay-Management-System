8a4be89839bd1f1a5b55ed5accf83ea51ee4ec79
jinoestillore-infotech <jinoestillore.infotech@gmail.com>
2026-06-12 01:39:08 +0800

    Modified authentication class and added logout function/method

--- classes/Authentication.php
+++ classes/Authentication.php
@@ -9,10 +9,7 @@ class Authentication
         $this->conn = $db;
     }
 
-    public function login(
-        $username,
-        $password
-    )
+    public function login($username, $password)
     {
         $stmt = $this->conn->prepare("
             SELECT *
@@ -20,30 +17,53 @@ class Authentication
             WHERE username = ?
             LIMIT 1
         ");
-        $stmt->bind_param(
-            "s",
-            $username
-        );
+
+        $stmt->bind_param("s", $username);
         $stmt->execute();
-        $user = $stmt
-            ->get_result()
-            ->fetch_assoc();
-        if (
-            $user &&
-            password_verify(
-                $password,
-                $user['password']
-            )
-        ) {
-            session_regenerate_id(true);
-            $_SESSION['user_id']
-                = $user['id'];
-            $_SESSION['fullname']
-                = $user['fullname'];
-            $_SESSION['role']
-                = $user['role'];
-            return true;
+
+        $user = $stmt->get_result()->fetch_assoc();
+
+        if (!$user) {
+            return false;
         }
-        return false;
+
+        if ($user['status'] !== 'Active') {
+            return false;
+        }
+
+        if (!password_verify($password, $user['password'])) {
+            return false;
+        }
+
+        session_regenerate_id(true);
+
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['fullname'] = $user['fullname'];
+        $_SESSION['username'] = $user['username'];
+        $_SESSION['role'] = $user['role'];
+        $_SESSION['last_activity'] = time();
+
+        return true;
+    }
+
+    public function logout()
+    {
+        $_SESSION = [];
+
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+
+            setcookie(
+                session_name(),
+                '',
+                time() - 42000,
+                $params["path"],
+                $params["domain"],
+                $params["secure"],
+                $params["httponly"]
+            );
+        }
+
+        session_destroy();
     }
 }
